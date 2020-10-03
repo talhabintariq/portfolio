5d0c5e64d3d42a956c62a958569660155b943d2e
Talha <talhabintariq@gmail.com>
2020-10-03 11:07:31 -0400

    test cicd

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -29,7 +29,6 @@ const Home = () => {
           </div>
           <div className="mainContainer">
             <div className="greeting-wrapper">
-              <div>Test Test Test</div>
               <h1>{translate('hi')}</h1>
             </div>
 
@@ -38,6 +37,7 @@ const Home = () => {
                 <div className="dots-wrapper">
                   <div id="dot-1" className="browser-dot" />
                   <div id="dot-2" className="browser-dot" />
+                  <div id="dot-3" className="browser-dot" />
                   <div id="dot-3" className="browser-dot" />
                 </div>
 
--- src/i18nProvider/messages/en-US.js
+++ src/i18nProvider/messages/en-US.js
@@ -3,7 +3,7 @@ import { LOCALES } from '../constants';
 
 export default {
   [LOCALES.ENGLISH]: {
-    'hi': "Hi, I'm Talha B. Tariq",
+    'hi': "Hiiiii, I'm Talha B. Tariq",
     'contact': 'Contact',
     'whatIDo': 'What I Do',
     'iBuild': 'I build things for the web.',
